ccbeaa142167bb275f87670883b7f343fb5a3e49
LDS_XiaoYe <xiaoyecreate@outlook.com>
2023-01-26 22:32:48 +0800

    支持 \n 多行消息，晕了

--- src/main/java/com/mcstaralliance/starjoinmessage/listener/PlayerJoinListener.java
+++ src/main/java/com/mcstaralliance/starjoinmessage/listener/PlayerJoinListener.java
@@ -9,26 +9,52 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-import java.util.List;
+import java.util.*;
 
 
 public class PlayerJoinListener implements Listener {
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent e){
-        FileConfiguration config = StarJoinMessage.getInstance().getConfig();
+    public void onPlayerJoin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
+        if(player.isOp())
+            return;
+        FileConfiguration config = StarJoinMessage.getInstance().getConfig();
         List<String> permissions = config.getStringList("settings.permissions");
         List<String> messages = config.getStringList("settings.messages");
+        List<String> processedMessages = new ArrayList<>();
+        processedMessages = processMultiline(messages, processedMessages);
+        processSend(permissions, processedMessages, player);
+    }
 
-
-        for(int i = 0;i < permissions.size();i++){
-            if(player.hasPermission(permissions.get(i))){
-                String messageToSend = messages.get(i).replaceAll("%player%", e.getPlayer().getName());
-                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',messageToSend));
+    public void processSend(List<String> permissions, List<String> processedMessages, Player player){
+        for (String permission : permissions) {
+            if (player.hasPermission(permission)) {
+                for (String processedMessage : processedMessages) {
+                    String messageToSend = processedMessage.replaceAll("%player%", player.getName());
+                    Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', messageToSend));
+                }
                 return;
             }
         }
+    }
 
+    public boolean isMultiline(List<String> messages){
+        for (String message : messages) {
+            return message.contains("\n");
+        }
+        return false;
+    }
+
+    public List<String> processMultiline(List<String> messages, List<String> processed) {
+        for (int i = 0; i < messages.size(); i++) {
+            if (isMultiline(messages)) {
+                Collections.addAll(processed, messages.get(i).split("\n"));
+            }
+        }
+        return processed;
     }
 }
+
+
+
